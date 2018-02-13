9eb8836fdbd8839e50d7828e5aa9995dd37a2e89
Julian C. Dunn <jdunn@chef.io>
2018-02-12 17:11:41 -0800

    Added SuSE support; fixed Darwin version expression
    Signed-off-by: Julian C. Dunn <jdunn@chef.io>

--- controls/chef.rb
+++ controls/chef.rb
@@ -6,15 +6,22 @@ title 'chef client installed and up to date'
 control 'chef-client installed and a non-EOL version' do
   impact 1.0
   title 'Make sure Chef Client is installed and a non-EOL version'
-  if os[:family] == :linux # TODO: package resource doesn't work on SuSE
-    describe package('chef') do
-      it { should be_installed }
-      its('version') { should match /^1[3-9]/ }
+  if os[:family] == :linux
+    if os.suse? # package resource not supported yet on SUSE
+      describe command('zypper product-info package:chef') do
+        its('stdout') { should_not match /product.*not found/ }
+        its('stdout') { should match /Version\s+:\s+1[3-9]/ }
+      end
+    else
+      describe package('chef') do
+        it { should be_installed }
+        its('version') { should match /^1[3-9]/ }
+      end
     end
   elsif os[:family] == :darwin
     describe command('pkgutil --pkg-info com.getchef.pkg.chef') do
       its('stderr') { should_not match /No receipt/ }
-      its('stdout') { should_not match /version: [0-1]/ }
+      its('stdout') { should match /version: 1[3-9]/ }
     end
   elsif os[:family] == :windows
     describe powershell('Get-Package -Name "Chef Client*" | Select-Object Version | Format-List') do
